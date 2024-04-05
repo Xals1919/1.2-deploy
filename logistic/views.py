d78b0c01268d329f0b55d5c44e890d59ec144fc4
Xals1919 <135830847+Xals1919@users.noreply.github.com>
2024-04-05 09:30:56 +0300

    Update views.py

--- logistic/views.py
+++ logistic/views.py
@@ -1,5 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import api_view
 from rest_framework.filters import SearchFilter
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from logistic.models import Product, Stock
@@ -18,3 +20,7 @@ class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
     filterset_fields = ['products',]
+
+@api_view(['GET'])
+def some_function(request):
+    return Response({'message': 'Hello world'})
