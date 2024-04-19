58deb66f51ba643fd6ac7d72196017d3dffaf282
Xals1919 <135830847+Xals1919@users.noreply.github.com>
2024-04-19 15:25:38 +0300

    Update urls.py

--- stocks_products/urls.py
+++ stocks_products/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from logistic.views import some_function
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('logistic.urls')),
+    path('api/v1/test', some_function),
 ]
